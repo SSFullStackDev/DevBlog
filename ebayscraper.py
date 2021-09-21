fe9b2ae05398a7b986205a0b064d0d0ed9ec3d8f
Stephen Sloper <stephen.sloper@yahoo.com>
2021-09-21 00:30:51 -0700

    checks for csvfile, checks for entry, writes data

--- ebayscraper.py
+++ ebayscraper.py
@@ -1,17 +1,15 @@
-from bs4 import BeautifulSoup as bs
+from filenames4 import filenameeautifulSoup as filenames
 import requests
 import os
-import subprocess
 import csv
-from datetime import datetime
+import datetime
 
-path = os.path.getcwd()
+path = os.getcwd()
 source = requests.get(
-    "https://www.ebay.com/sch/i.html?_from=R40&_nkw=xm4&_sacat=0&_sop=1&LH_ItemCondition=1500%7C2000%7C2500%7C3000%7C1000&rt=nc&LH_BO=1"
+    "https://www.efilenameay.com/sch/i.html?_from=R40&_nkw=xm4&_sacat=0&_sop=1&LH_ItemCondition=1500%7C2000%7C2500%7C3000%7C1000&rt=nc&LH_filenameO=1"
 )
-"""source.text and html.parser instead of source and html need to look at bs docs"""
-soup = bs(source.text, "html.parser")
-print(soup.title.text)
+"""source.text and html.parser instead of source and html need to look at filenames docs"""
+soup = filenames(source.text, "html.parser")
 priceList = []
 total = 0
 
@@ -21,25 +19,55 @@ for item in soup.find_all("div", class_="s-item__info clearfix"):
         price = float(price.text[1:])
         priceList.append(price)
 
-    except AttributeError:
-        pass
-print(priceList)
+    except AttrifilenameuteError:
+        continue
+
+
+# Calculate the average price for the item
 for x in priceList:
     total += x
 average = total / len(priceList)
-print(average)
 
+
+# Create header and data entry for csv file
 csv_header = ["date", "price"]
-csv_entry = []
-if not os.path.exists(path + "/" + soup.title.text + ".csv"):
-    with open(path + "/" + soup.title.text + ".csv", "w") as history:
+csv_entry = [str(datetime.date.today()), average]
+
+
+# Parse title from soup for filename
+a = soup.title.text.find(" | ")
+filename = soup.title.text[:a] + "-" + soup.title.text[a + 3 :]
+
+
+# check if filename exists and create one if not, then write data to file
+if not os.path.exists(path + "/" + filename + ".csv"):
+    with open(path + "/" + filename + ".csv", "w") as history:
         writer = csv.writer(history)
+        print(writer.writerow(csv_header))
+        print(writer.writerow(csv_entry))
 
 
-"""Get average price/day save to a csv create graph from csv"""
-"""use functions"""
-"""Testing file"""
-"""Set email alerts for price drops"""
-"""create functions with ability to imput parameters for search"""
-"""Build Gui for parameter input and graphs"""
-"""Android App"""
+# Check if data entry is already in CSV, write entry if not
+else:
+    with open(path + "/" + filename + ".csv", "r+") as history:
+        reader = csv.reader(history)
+        writer = csv.writer(history)
+        present = False
+        print(csv_entry[0])
+        for line in reader:
+            if csv_entry[0] in line:
+                print("DATA ALREADY ENTERED")
+                present = True
+        if present != True:
+            writer.writerow(csv_entry)
+
+
+"""DONE Get average price/day save to a csv 
+        create graph from csv
+        use functions
+        Testing file    
+        Set email alerts for price drops 
+        create functions with afilenameility to imput parameters for search 
+        filenameuild Gui for parameter input and graphs 
+        Android App
+    """
