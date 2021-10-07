19b1614d99c3e29ab6394698402f2c7fb46efccf
stephen sloper <stephen.sloper@yahoo.com>
2021-10-07 00:43:14 -0700

    created model for price_history database

--- price_tracker/trackerapp/admin.py
+++ price_tracker/trackerapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import price_history
 
-# Register your models here.
+admin.site.register(price_history)
--- price_tracker/trackerapp/models.py
+++ price_tracker/trackerapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class price_history(models.Model):
+    title = models.CharField(max_length=20)
+    data = models.JSONField(default = {})
+
+    def __str__(self):
+        return self.title
