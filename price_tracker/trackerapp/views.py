d83fd5008f9d9f331cb33ae384975ca7667bf23f
Stephen Sloper <stephen.sloper@yahoo.com>
2021-10-07 00:55:55 -0700

    display data

--- price_tracker/trackerapp/views.py
+++ price_tracker/trackerapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import price_history
 
 
 def home(request):
-    return render(request, "trackerapp/home.html")
+    data = price_history.objects.all
+    return render(request, "trackerapp/home.html", {"data": data})
 
 
 def about(request):
