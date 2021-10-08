cec0dce6eb7c0aaee46fac3a291dc4ce3f3e5afc
stephen sloper <stephen.sloper@yahoo.com>
2021-10-07 17:15:59 -0700

    json filed updated

--- price_tracker/trackerapp/models.py
+++ price_tracker/trackerapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class price_history(models.Model):
     title = models.CharField(max_length=20)
-    data = models.JSONField(default = {})
+    data = models.JSONField(default = [])
 
     def __str__(self):
         return self.title
